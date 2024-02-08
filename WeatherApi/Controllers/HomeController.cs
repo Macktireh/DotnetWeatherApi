e457f9e385b3b31c48cd7457da17487e90f45de8
Macktireh <abdimack97@gmail.com>
2024-02-08 23:03:42 +0100

    Redirect home page to swagger

---
+++ WeatherApi/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public RedirectResult Swagger()
+    {
+        return Redirect("/swagger/index.html");
+    }
+}
