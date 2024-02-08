6e403e0d5de277d37c09b58ca964fdbba7da5f69
Macktireh <abdimack97@gmail.com>
2024-02-08 23:06:11 +0100

    Add weather models

---
+++ WeatherApi/Models/SearchLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherApi.Models;
+
+public class SearchLocation
+{
+
+    public int id { get; set; }
+    public string name { get; set; }
+    public string region { get; set; }
+    public string country { get; set; }
+    public double lat { get; set; }
+    public double lon { get; set; }
+    public string url { get; set; }
+
+}
---
+++ WeatherApi/Models/Weather.cs
@@ -0,0 +1,145 @@
+﻿namespace WeatherApi.Models;
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+public class Astro
+{
+    public string sunrise { get; set; }
+    public string sunset { get; set; }
+    public string moonrise { get; set; }
+    public string moonset { get; set; }
+    public string moon_phase { get; set; }
+    public int moon_illumination { get; set; }
+    public int is_moon_up { get; set; }
+    public int is_sun_up { get; set; }
+}
+
+public class Condition
+{
+    public string text { get; set; }
+    public string icon { get; set; }
+    public int code { get; set; }
+}
+
+public class Current
+{
+    public int last_updated_epoch { get; set; }
+    public string last_updated { get; set; }
+    public int temp_c { get; set; }
+    public double temp_f { get; set; }
+    public int is_day { get; set; }
+    public Condition condition { get; set; }
+    public double wind_mph { get; set; }
+    public double wind_kph { get; set; }
+    public int wind_degree { get; set; }
+    public string wind_dir { get; set; }
+    public int pressure_mb { get; set; }
+    public double pressure_in { get; set; }
+    public double precip_mm { get; set; }
+    public double precip_in { get; set; }
+    public int humidity { get; set; }
+    public int cloud { get; set; }
+    public double feelslike_c { get; set; }
+    public double feelslike_f { get; set; }
+    public int vis_km { get; set; }
+    public int vis_miles { get; set; }
+    public int uv { get; set; }
+    public double gust_mph { get; set; }
+    public double gust_kph { get; set; }
+}
+
+public class Day
+{
+    public double maxtemp_c { get; set; }
+    public int maxtemp_f { get; set; }
+    public double mintemp_c { get; set; }
+    public double mintemp_f { get; set; }
+    public double avgtemp_c { get; set; }
+    public double avgtemp_f { get; set; }
+    public int maxwind_mph { get; set; }
+    public double maxwind_kph { get; set; }
+    public double totalprecip_mm { get; set; }
+    public double totalprecip_in { get; set; }
+    public int totalsnow_cm { get; set; }
+    public double avgvis_km { get; set; }
+    public int avgvis_miles { get; set; }
+    public int avghumidity { get; set; }
+    public int daily_will_it_rain { get; set; }
+    public int daily_chance_of_rain { get; set; }
+    public int daily_will_it_snow { get; set; }
+    public int daily_chance_of_snow { get; set; }
+    public Condition condition { get; set; }
+    public int uv { get; set; }
+}
+
+public class Forecast
+{
+    public List<Forecastday> forecastday { get; set; }
+}
+
+public class Forecastday
+{
+    public string date { get; set; }
+    public int date_epoch { get; set; }
+    public Day day { get; set; }
+    public Astro astro { get; set; }
+    public List<Hour> hour { get; set; }
+}
+
+public class Hour
+{
+    public int time_epoch { get; set; }
+    public string time { get; set; }
+    public double temp_c { get; set; }
+    public double temp_f { get; set; }
+    public int is_day { get; set; }
+    public Condition condition { get; set; }
+    public double wind_mph { get; set; }
+    public double wind_kph { get; set; }
+    public int wind_degree { get; set; }
+    public string wind_dir { get; set; }
+    public int pressure_mb { get; set; }
+    public double pressure_in { get; set; }
+    public double precip_mm { get; set; }
+    public double precip_in { get; set; }
+    public int snow_cm { get; set; }
+    public int humidity { get; set; }
+    public int cloud { get; set; }
+    public double feelslike_c { get; set; }
+    public double feelslike_f { get; set; }
+    public double windchill_c { get; set; }
+    public double windchill_f { get; set; }
+    public double heatindex_c { get; set; }
+    public double heatindex_f { get; set; }
+    public double dewpoint_c { get; set; }
+    public double dewpoint_f { get; set; }
+    public int will_it_rain { get; set; }
+    public int chance_of_rain { get; set; }
+    public int will_it_snow { get; set; }
+    public int chance_of_snow { get; set; }
+    public int vis_km { get; set; }
+    public int vis_miles { get; set; }
+    public double gust_mph { get; set; }
+    public double gust_kph { get; set; }
+    public int uv { get; set; }
+}
+
+public class Location
+{
+    public string name { get; set; }
+    public string region { get; set; }
+    public string country { get; set; }
+    public double lat { get; set; }
+    public double lon { get; set; }
+    public string tz_id { get; set; }
+    public int localtime_epoch { get; set; }
+    public string localtime { get; set; }
+}
+
+public class Weather
+{
+    public Location location { get; set; }
+    public Current current { get; set; }
+    public Forecast forecast { get; set; }
+}
+
+
