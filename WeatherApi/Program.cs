1e60d4857ff2e60bc7e10e994882e67f262a6de6
Macktireh <abdimack97@gmail.com>
2024-02-11 09:40:52 +0100

    swagger configurator for production

--- WeatherApi/Program.cs
+++ WeatherApi/Program.cs
@@ -14,14 +14,14 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
+// {
+// }
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather API V1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather API V1");
+});
 
 app.UseHttpsRedirection();
 
